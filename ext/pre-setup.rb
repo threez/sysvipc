076df38971f9274426c2424eeac7e22b13ac45eb
James Steven Jenkins <steven.jenkins@ieee.org>
2007-09-13 22:00:29 +0000

    Turn off unnecessary warnings.

--- ext/pre-setup.rb
+++ ext/pre-setup.rb
@@ -5,5 +5,5 @@ cfile = basename + '.c'
 
 unless File.file?(cfile) and
     File.mtime(cfile) >= File.mtime(ifile)
-	system "swig -ruby -o #{cfile} #{ifile}"
+	system "swig -ruby -w-801 -o #{cfile} #{ifile}"
 end
